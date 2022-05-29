12bd8a062879985f849e3aa0c7e47a17f7bc2f23
gojeongseog <94058164+gojeongseog@users.noreply.github.com>
2022-05-29 16:49:32 +0900

    Create a project

---
+++ CalculatorClone/CalculatorClone/CalculatorCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorCloneApp.swift
+//  CalculatorClone
+//
+//  Created by KoJeongseok on 2022/05/29.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
